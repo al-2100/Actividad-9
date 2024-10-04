3dfe7b5d7f01f69c11ba4af1a68ab416384d954d
Jorge Barriga <alonsoxbarriga@hotmail.com>
2024-10-04 01:38:14 -0500

    implementar `adivinar_letra()`

--- src/game_logic.py
+++ src/game_logic.py
@@ -5,3 +5,12 @@ def seleccionar_palabra():
         palabras = file.readlines()
     print("La palabra ha sido seleccionada. ¡Comienza a adivinar!")
     return random.choice(palabras).strip()
+
+def adivinar_letra(letra, palabra, progreso,intentos_restantes):
+    if letra in palabra:
+        for i in range(len(palabra)):
+            if palabra[i] == letra:
+                progreso[i] = letra
+        return True, intentos_restantes
+    else:
+        return False, intentos_restantes - 1
